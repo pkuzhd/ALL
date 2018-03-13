29e7175a8cf83adcf97666c3f4318e992e13a2cf
pkuzhd <pkuzhd@pku.edu.cn>
2018-03-13 23:11:09 +0800

    20180313

--- Python/learn.py
+++ Python/learn.py
@@ -1,6 +1,11 @@
-d = dict()
-d['one']=1
-d['two']=2
-d['three']=3
-d['four']=4
-print(d)
+s1 = input()
+s2 = input()
+
+def f(s1, s2):
+    union = set()
+    u1 = set(s1)
+    u2 = set(s2)
+    union = u1 | u2
+    return sorted(union)
+
+print(f(s1, s2))
